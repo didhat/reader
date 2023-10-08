2b129540ac89f1c60e7ecc1b9b595d12d2943324
Daniil Solynin <didhat82@gmail.com>
2023-10-08 17:34:37 +0300

    add migration for create book table

---
+++ src/infrastructure/db/tables/book.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+
+from src.infrastructure.db.tables.base import meta
+
+
+book_table = Table(
+    "book",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(100)),
+    Column("author", String(100)),
+    Column("chapter_number", Integer),
+    Column("added_time", DateTime, server_default=func.now())
+)
+
