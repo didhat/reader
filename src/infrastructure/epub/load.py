30ed3a13cd20557b7fcc3904b9843f47329a79d5
Daniil Solynin <didhat82@gmail.com>
2023-10-11 08:17:17 +0300

    fix file breaking

--- src/infrastructure/epub/load.py
+++ src/infrastructure/epub/load.py
@@ -1,3 +1,5 @@
+import shutil
+
 import zipfile
 from tempfile import SpooledTemporaryFile
 from ebooklib import epub
@@ -6,17 +8,38 @@ from src.infrastructure.epub.sorted import EpubBookWithMethods
 
 
 def load_epub_from_upload(file: SpooledTemporaryFile, filename: str):
-    file = zipfile.ZipFile(file, "r", compression=zipfile.ZIP_DEFLATED, allowZip64=True)
-
-    reader = epub.EpubReader(filename, {"ignore_ncx": True})
-    reader.zf = file
-    reader._load_container()
-    reader._load_opf_file()
-
-    file.close()
+    reader = EpubBookWithUploadFromBinary.from_binary(file, filename)
 
     book = reader.book
-
     reader.process()
 
     return EpubBookWithMethods(book)
+
+
+class EpubBookWithUploadFromBinary(epub.EpubReader):
+    @classmethod
+    def from_binary(cls, file: SpooledTemporaryFile, filename: str):
+        file_copy = create_file_deepcopy(file)
+        reader = cls(filename, {"ignore_ncx": True})
+
+        zip_file = zipfile.ZipFile(
+            file_copy, "r", compression=zipfile.ZIP_DEFLATED, allowZip64=True
+        )
+
+        reader.zf = zip_file
+
+        reader._load_container()
+        reader._load_opf_file()
+
+        zip_file.close()
+        file_copy.close()
+
+        return reader
+
+
+def create_file_deepcopy(file: SpooledTemporaryFile):
+    file_copy = SpooledTemporaryFile(mode="wb+")
+    shutil.copyfileobj(file, file_copy)
+    file_copy.seek(0)
+    file.seek(0)
+    return file_copy
