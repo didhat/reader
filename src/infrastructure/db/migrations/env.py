eaa448208873effe18695b82344dce0ffb500943
Daniil Solynin <didhat82@gmail.com>
2023-10-14 10:22:02 +0300

    fix mypy

--- src/infrastructure/db/migrations/env.py
+++ src/infrastructure/db/migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.infrastructure.db.tables import meta
+from src.infrastructure.db.tables.base import meta
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
